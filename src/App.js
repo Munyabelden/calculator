372b61537846e3b55454c60076e8fcea044cacbf
Gracerue <munyaradzi045@gmail.com>
2023-03-30 17:29:50 +0200

    Fetch qoute from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Calculator from './components/Calculator';
+import Qoute from './components/displayQoute';
 
 function App() {
   return (
     <div className="App">
+      <Qoute />
       <Calculator />
     </div>
   );
