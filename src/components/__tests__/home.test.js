128cb5a659f17f879d3c9df149512e47e0b8194d
Gracerue <graceruvimbo681@gamil.com>
2023-04-05 18:31:31 +0200

    Add home test

---
+++ src/components/__tests__/home.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "../home";
+​
+test("renders home page content", () => {
+  render(<Home />);
+​
+  expect(screen.getByText("Welcome to our page!")).toBeInTheDocument();
+  expect(screen.getByText(/Lorem Ipsum is simply dummy text/)).toBeInTheDocument();
+  expect(screen.getByText(/Contrary to popular belief/)).toBeInTheDocument();
+});
